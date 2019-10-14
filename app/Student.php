56bc404fff2e308b09d7f958a97d13230a1db1e9
rekhabk <rekha2017n@gmail.com>
2019-10-14 10:04:31 +0530

    initial release

---
+++ app/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    Protected $fillable=['firstname','lastname','gender','country','city','address'];
+    Protected $primaryKey='id';
+}
+
